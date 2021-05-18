10fd507cf35ad80bfd80ad7b20441186baca048d
lmagarin <lmagarin@users.noreply.github.com>
2021-05-18 20:13:01 +0200

    docs: documentacion javadoc EdadErroneaException - Lourdes Magarín

--- pgn/exUD4/EdadErroneaException.java
+++ pgn/exUD4/EdadErroneaException.java
@@ -1,7 +1,17 @@
 package pgn.exUD4;
 
+/**
+ * Lanzada para indicar que una edad es errónea
+ * 
+ * @author María Lourdes Magarín Corvillo
+ *
+ */
 public class EdadErroneaException extends Exception {
-
+	/**
+	 * Construye una EdadErroneaException con el mensaje especificado
+	 * 
+	 * @param string mensaje de la excepción
+	 */
 	public EdadErroneaException(String string) {
 		super(string);
 	}
