8e0fb500bb7ea0dc4de67f0132d6060e3e76d563
lmagarin <lmagarin@users.noreply.github.com>
2021-05-22 14:04:35 +0200

    docs: javadoc generada documentación

--- pgn/exUD4/EdadErroneaException.java
+++ pgn/exUD4/EdadErroneaException.java
@@ -8,7 +8,7 @@ package pgn.exUD4;
  */
 public class EdadErroneaException extends Exception {
 	/**
-	 * Construye una EdadErroneaException con el mensaje especificado
+	 * Construye una excepción EdadErroneaException con el mensaje especificado
 	 * 
 	 * @param string mensaje de la excepción
 	 */
